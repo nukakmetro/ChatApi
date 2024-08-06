4446219de95ede2c188e9ac0f4ab1773b99e0cfb
Aidar <112638211+nukakmetro@users.noreply.github.com>
2024-08-06 21:39:23 +0300

    added initial version

---
+++ ChatApi/src/main/java/org/example/chatapi/ChatApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.chatapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApiApplication.class, args);
+    }
+
+}
